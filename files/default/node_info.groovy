08704debe47171a8ee5decf9d6330c53a5a16984
Thomas Kadauke <tkadauke@imedo.de>
2013-02-07 12:29:57 +0100

    Fix syntax error in script

--- files/default/node_info.groovy
+++ files/default/node_info.groovy
@@ -1,3 +1,4 @@
+/*
 #
 # Cookbook Name:: jenkins
 # Based on hudson
@@ -20,6 +21,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+*/
 
 import jenkins.slaves.*
 import jenkins.model.*
